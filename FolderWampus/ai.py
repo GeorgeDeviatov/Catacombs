b253ff6d84b4e82caff4360591d041c298f39d46
GeorgeDeviatov <91205479+GeorgeDeviatov@users.noreply.github.com>
2022-01-06 20:44:42 +0800

    Player update
    Added player.

--- FolderWampus/ai.py
+++ FolderWampus/ai.py
@@ -1,9 +1,12 @@
+import pygame as pg
 class AI:
     def __init__(self,agent):
         self.agent = agent
+        self.tick = 0
+        self.pl = False
     
     def step(self):
-        self.agent.mes = self.agent.env.get_mes()
+        self.get_direction()
         if self.agent.mes == None or self.agent.mes == 'n':
             if self.agent.direction == 0:
                 self.agent.pos[1] -= 1
@@ -24,6 +27,7 @@ class AI:
             self.agent.ret()
         elif self.agent.mes == 'h':
             self.agent.ret()
+        self.tick = 500
     
     def get_direction(self):# 0 - up, 1 - right, 2 - down, 3 - left, 4 - back
         if self.agent.pos[1]-1 >= 0 and self.agent.ag_map[self.agent.pos[0]][self.agent.pos[1]-1] == 'n':
@@ -36,3 +40,30 @@ class AI:
             self.agent.direction = 1
         else:
             self.agent.direction = 4
+
+class Player:
+    def __init__(self,agent):
+        self.agent = agent
+        self.tick=0
+        self.pl = True
+    
+    def step(self):
+        keys = pg.key.get_pressed()
+        if keys[pg.K_s] == True:
+            if self.agent.pos[1]+1<self.agent.env.height:
+                self.agent.pos[1] += 1
+                self.tick = 1000
+        if keys[pg.K_w] == True:
+            if self.agent.pos[1]>0:
+                self.agent.pos[1]-=1
+                self.tick = 1000
+        if keys[pg.K_d] == True:
+            if self.agent.pos[0]+1 < self.agent.env.width:
+                self.agent.pos[0]+=1
+                self.tick = 1000
+        if keys[pg.K_a] == True:
+            if self.agent.pos[0]>0:
+                self.agent.pos[0]-=1
+                self.tick = 1000
+        self.agent.ag_map[self.agent.pos[0]][self.agent.pos[1]] = 'x'
+        self.agent.path.append(self.agent.pos.copy())
